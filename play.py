3e4e7af9c62ae40eda4aa3eba0c7465b56d84eb7
David Morison <davemorison@gmail.com>
2021-03-25 15:41:54 +0000

    getting apply lambda function for cols to work

--- play.py
+++ play.py
@@ -13,12 +13,28 @@ df = df.rename_axis('week')
 print(df)
 
 def other_func(x):
-  return x * 10
+  if x < 3:
+    return x
+  else:
+    return x * 10
 
-def times_function(x):
-  global other_func
-  return np.where(x < 3, x, other_func(x))
+# def times_function(x):
+  # global other_func
+  # return np.where(x < 3, x, other_func(x))
+
+def test_func(x):
+  result = x.apply(lambda x: other_func(x))
+  return result
+  # print(x)
+  # x = x.tolist()
+  # print(x)
+  # for i in x:
+  #   print(i)
+  #   if i < 3:
+  #     return i
+  #   else:
+  #     return i * 10
 
 time_cols = ['oranges', 'kiwis']
-df1 = df.apply(lambda x: times_function(x) if x.name in time_cols else x)
+df1 = df.apply(lambda x: test_func(x) if x.name in time_cols else x)
 print(df1)
--- script.py
+++ script.py
@@ -28,12 +28,22 @@ def times_function(x):
       minutes = int(hms_arr[0]) * 60
     return hours + minutes + seconds
 
+def series_other_function(x):
+  global other_function
+  return x.apply(lambda y: other_function(y))
+
+def series_time_function(x):
+  global times_function
+  return x.apply(lambda y: times_function(y))
+
 def processDataFunction(sport):
   global col_names_function
-  global other_function
-  global times_function
+  # global other_function
+  # global times_function
+  global series_other_function
+  global series_time_function
 
-  df = pd.read_csv('./data_in/' + sport + '.csv', index_col='Time Period')
+  df = pd.read_csv('./data_in/' + sport + '.csv', index_col='Time Period', parse_dates=True)
   df = df.rename_axis('week')
 
   df = df[:-1]
@@ -58,14 +68,9 @@ def processDataFunction(sport):
     time_df = df[['total_activity_time', 'avg_time']]
     other_df = df[['activities', 'activity_calories']]
   
-  # time_cols = ['total_activity_time', 'avg_time', 'max_time', 'average_pace']
-  # other_cols = ['activities', 'total_distance', 'average_distance', 'max_distance', 'activity_calories', 'total_elev_gain', 'avg_elevation_gain', 'max_elevation_gain', 'average_heart_rate']
-  # df1 = df.apply(lambda x: times_function(x) if x.name in time_cols else x)
-  # df1 = df.apply(lambda x: other_function(x) if x.name in other_cols else x)
-  df1 = time_df.applymap(times_function)
-  df2 = other_df.applymap(other_function)
-  
-  result = df1.join(df2)
+  time_cols = ['total_activity_time', 'avg_time', 'max_time', 'average_pace']
+  other_cols = ['activities', 'total_distance', 'average_distance', 'max_distance', 'activity_calories', 'total_elev_gain', 'avg_elevation_gain', 'max_elevation_gain', 'average_heart_rate']
+  result = df.apply(lambda x: series_time_function(x) if x.name in time_cols else series_other_function(x))
 
   if sport == "swim":
     result['average_pace'] = result['average_pace'].map(lambda x: x / 10)
@@ -73,7 +78,7 @@ def processDataFunction(sport):
   print(result.head())
   print("------------------")
 
-  # result.to_csv('./data_out/' + sport + '.csv', encoding='utf-8')
+  result.to_csv('./data_out/' + sport + '.csv', encoding='utf-8')
 
 sports = ["swim", "cycle", "run", "strength"]
 for sport in sports:
